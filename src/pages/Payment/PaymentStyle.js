b686db9bb54e739ddbb47061fecd600c30224631
minss7 <paul2723@snu.ac.kr>
2024-11-01 19:25:43 +0900

    FEAT: 결제 페이지 데스크탑 퍼블리싱

--- src/pages/Payment/PaymentStyle.js
+++ src/pages/Payment/PaymentStyle.js
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { useState } from "react";
 
 export const Wrap = styled("div")`
   display: flex;
@@ -30,6 +31,7 @@ export const MiddleWrapper = styled.div`
   justify-content: center;
   gap: 20px;
   width: 100%;
+  align-items: flex-start;
 `;
 
 export const OrderInformationWrap = styled.div`
@@ -74,7 +76,7 @@ export const TwoTextWrapper = styled.div`
   gap: 10px;
 `;
 
-export const TextWrapper = styled.div`
+export const TextWrapper = styled.input`
   display: flex;
   border-radius: 20px;
   background: rgba(215, 215, 215, 0.3);
@@ -82,19 +84,115 @@ export const TextWrapper = styled.div`
   padding-left: 15px;
   align-items: center;
   color: #d7d7d7;
-  font-size: 10px;
+  font-size: 13px;
   font-style: normal;
   font-weight: 400;
   line-height: 100%;
   margin-bottom: 12px;
   height: 43px;
+  &::placeholder {
+    color: #d7d7d7;
+  }
+  border: none;
+  outline: none;
 `;
 
 export const OrderDetailWrap = styled.div`
   width: 43%;
-  height: 100px;
+  height: auto;
   border-radius: 20px;
   background: rgba(215, 215, 215, 0.3);
+  display: block;
+  box-sizing: border-box;
+`;
+
+export const OrderText = styled.div`
+  color: #a348f6;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  margin-left: 28px;
+  margin-top: 34px;
+  margin-bottom: 10px;
+`;
+
+export const Line = styled.div`
+  background: #a348f6;
+  margin: 18px 28px;
+  height: 2px;
+`;
+
+export const OrderMidWrap = styled.div`
+  display: flex;
+  margin-left: 55px;
+  margin-right: 60px;
+`;
+
+export const LeftWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+export const ArtImage = styled.img`
+  height: 75px;
+  width: 75px;
+  border-radius: 20px;
+  background: rgba(217, 217, 217, 0.3);
+  backdrop-filter: blur(50px);
+`;
+
+export const ArtTextWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  justify-content: center;
+`;
+
+export const ArtText = styled.span`
+  color: #ddd;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 100%;
+`;
+export const ArtInfo = styled.span`
+  color: #fff;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 100%;
+`;
+
+export const ArtPrice = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  gap: 3px;
+`;
+
+export const PurpleText = styled.span`
+  color: #a348f6;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 100%;
+`;
+
+export const WhiteText = styled.span`
+  color: #fff;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 100%;
+`;
+
+export const TotalWrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 102px;
+  gap: 5px;
+  margin-bottom: 39px;
+  margin-top: 4px;
 `;
 
 export const PaymentButton = styled.button`
@@ -108,11 +206,11 @@ export const PaymentButton = styled.button`
   align-items: center;
   gap: 10px;
   flex-shrink: 0;
-  color: #fff;
-  font-family: "KoddiUD OnGothic";
+  color: ${({ isComplete }) => (isComplete ? "black" : "white")};
   font-size: 25px;
   font-style: normal;
   font-weight: 700;
-  background-color: var(--black);
+  background-color: ${({ isComplete }) =>
+    isComplete ? "#fae100" : "var(--black)"};
   margin-top: 50px;
 `;
