01eff3d1918122988c73acb58686ef4d91a0278f
Daeyu <paull07@snu.ac.kr>
2024-11-02 13:37:06 +0900

    [Feat] initial route setting

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -4,6 +4,7 @@ import NotFound from "../pages/NotFound/NotFound.jsx";
 import Department from "../pages/Department/Department.jsx";
 import Landing from "../pages/Landing/Landing.jsx";
 import DeptDetail from "../pages/DeptDetail/DeptDetail.jsx";
+import Artwork from "../pages/Artwork/Artwork";
 function Router() {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ function Router() {
         <Route path="/" element={<Landing />}></Route>
         <Route path="/:department" element={<Department />}></Route>
         <Route path="/dept_detail/:dept_id" element={<DeptDetail />}></Route>
+          <Route path="/dept_detail/:dept_id/:artwork_id" element={<Artwork />}></Route>
         <Route exact path="/*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Artwork/Artwork.jsx
@@ -0,0 +1,11 @@
+import {DepartmentHeader} from "../../components/DepartmentHeader/DepartmentHeader";
+
+function Artwork () {
+    return(
+        <>
+            <DepartmentHeader></DepartmentHeader>
+        </>
+    );
+}
+
+export default Artwork
