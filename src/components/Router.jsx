95f759aaa6ad5630d7de705b90e217307b53b636
조유진 <uzinfamily@snu.ac.kr>
2024-11-07 04:12:20 +0900

    Feat: PaymentApproval 데스크탑/모바일 퍼블리싱

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -6,11 +6,12 @@ import Landing from "../pages/Landing/Landing.jsx";
 
 import Refund from "../pages/Refund/Refund";
 import Payment from "../pages/Payment/Payment.jsx";
+import PaymentApproval from "../pages/PaymentApproval/PaymentApproval.jsx";
 
 import DeptDetail from "../pages/DeptDetail/DeptDetail.jsx";
 import Artwork from "../pages/Artwork/Artwork.jsx";
 import PaymentSuccess from "../pages/PaymentSuccess/PaymentSuccess.jsx";
-import SignIn from '../pages/SignIn/SignIn.jsx';
+import SignIn from "../pages/SignIn/SignIn.jsx";
 import My from "../pages/My/My.jsx";
 import Cart from "../pages/Cart/Cart.jsx";
 
@@ -19,18 +20,18 @@ function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />}></Route>
-          <Route path="/artwork/:artwork_id" element={<Artwork />}></Route>
+        <Route path="/artwork/:artwork_id" element={<Artwork />}></Route>
         <Route path="/:department" element={<Department />}></Route>
         <Route exact path="/*" element={<NotFound />}></Route>
         <Route path="/refund" element={<Refund />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/dept_detail/:dept_id" element={<DeptDetail />}></Route>
+        <Route path="/payment-approval" element={<PaymentApproval />}></Route>
         <Route path="/payment-success" element={<PaymentSuccess />}></Route>
         <Route exact path="/*" element={<NotFound />}></Route>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/my" element={<My />} />
         <Route exact path="/cart" element={<Cart />}></Route>
-
       </Routes>
     </BrowserRouter>
   );
