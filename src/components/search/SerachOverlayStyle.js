261a59bf6ed4ccce48bcf0053bcaed46ad738949
minss7 <paul2723@snu.ac.kr>
2024-11-13 03:19:35 +0900

    FEAT: 검색창 퍼블리싱

--- src/components/search/SerachOverlayStyle.js
+++ src/components/search/SerachOverlayStyle.js
@@ -8,6 +8,88 @@ export const Container = styled.div`
   background-color: #d9d9d9;
 `;
 
+export const Head = styled.header`
+  align-items: center;
+  background-color: #d9d9d9;
+  display: flex;
+  height: 72px;
+  justify-content: space-between;
+  position: relative;
+  width: 100%;
+`;
+
+export const Logo = styled.div`
+  align-items: baseline;
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+  margin-left: 30px;
+  padding: 10px 10px 20px 0;
+  @media (max-width: 768px) {
+    align-items: center;
+    margin-left: 10px;
+  }
+`;
+export const TextWrapper = styled.span`
+  color: var(--purple);
+  font-family: "godorounded", sans-serif;
+  font-size: 40px;
+  font-weight: 400;
+  @media (max-width: 768px) {
+    font-size: 22px;
+  }
+`;
+
+export const Span = styled.span`
+  color: #8f8f8f;
+  font-family: "godo", sans-serif;
+  font-size: 20px;
+  line-height: 20px;
+`;
+
+export const Circles = styled.div`
+  align-items: center;
+  display: flex;
+  gap: 10px;
+  height: 72px;
+  justify-content: center;
+  margin-right: 30px;
+`;
+
+export const Ellipse = styled.div`
+  background-color: var(--purple);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+  height: 25px;
+  left: 0;
+  top: 0;
+  width: 25px;
+`;
+
+export const Ellipse2 = styled.div`
+  background-color: var(--light-gray);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+  height: 25px;
+  top: 0;
+  width: 25px;
+`;
+
+export const Ellipse3 = styled.div`
+  background-color: var(--purple);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+  height: 25px;
+  top: 0;
+  width: 25px;
+`;
+
 // 전체 작품 리스트 컨테이너
 export const ArtworkListContainer = styled.div`
   display: flex;
