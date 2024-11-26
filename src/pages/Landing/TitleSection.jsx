f913af61784b6d1c50e0b4d7914a78301a6b57b6
조유진 <uzinfamily@snu.ac.kr>
2024-11-27 08:12:08 +0900

    [FIX] main page mobile navigation, second font

--- src/pages/Landing/TitleSection.jsx
+++ src/pages/Landing/TitleSection.jsx
@@ -32,36 +32,16 @@ const TitleSection = () => {
     <TitleContainer>
       <TitleContent hovered={hoveredLetter}>
         <Row>
-          <LetterBlock
-          // onMouseEnter={() => handleHover("g")}
-          // onTouchEnd={() => handleHover("g")}
-          >
-            g
-          </LetterBlock>
+          <LetterBlock>g</LetterBlock>
           <LetterBlock>r</LetterBlock>
-          <LetterBlock
-          // onMouseEnter={() => handleHover("a")}
-          // onTouchEnd={() => handleHover("a")}
-          >
-            a
-          </LetterBlock>
+          <LetterBlock>a</LetterBlock>
         </Row>
         <Row>
-          <LetterBlock
-            onMouseEnter={() => handleHover("d")}
-            onTouchEnd={() => handleHover("d")}
-          >
-            d
-          </LetterBlock>
+          <LetterBlock onClick={() => handleHover("d")}>d</LetterBlock>
           <HighlightBlock>
             <HighlightBar />
           </HighlightBlock>
-          <LetterBlock
-            onMouseEnter={() => handleHover("u")}
-            onTouchEnd={() => handleHover("u")}
-          >
-            u
-          </LetterBlock>
+          <LetterBlock onClick={() => handleHover("u")}>u</LetterBlock>
         </Row>
         <Row>
           <LetterBlock>a</LetterBlock>
@@ -70,49 +50,15 @@ const TitleSection = () => {
         </Row>
       </TitleContent>
 
-      {hoveredLetter === "g" && (
-        <HoverContentLeft
-          onMouseLeave={handleLeave}
-          onTouchEnd={handleLeave}
-          rowPosition={1}
-        >
-          <PurpleCircleLeft />
-          <PaintingText>painting</PaintingText>
-          <HoverButton to={"/dept_detail/Ceramic"}>
-            <div className="hover-bg" />
-            <div className="hover-text">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={window.innerWidth > 768 ? 38 : 18}
-                height="26"
-                viewBox="0 0 38 26"
-                fill="none"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M22.7989 0.512563C23.4556 -0.170854 24.5205 -0.170854 25.1773 0.512563L36.95 12.7626C37.6068 13.446 37.6068 14.554 36.95 15.2374L25.1773 27.4874C24.5205 28.1709 23.4556 28.1709 22.7989 27.4874C22.1421 26.804 22.1421 25.696 22.7989 25.0126L31.7005 15.75H2.12445C1.1956 15.75 0.442627 14.9665 0.442627 14C0.442627 13.0335 1.1956 12.25 2.12445 12.25H31.7005L22.7989 2.98744C22.1421 2.30402 22.1421 1.19598 22.7989 0.512563Z"
-                  fill="#A348F6"
-                />
-              </svg>
-              <span className="dept-name">도예전공</span>
-              <span className="cta"> 작품 보러가기</span>
-            </div>
-          </HoverButton>
-        </HoverContentLeft>
-      )}
-
       {hoveredLetter === "d" && (
-        <HoverContentLeft
-          onMouseLeave={handleLeave}
-          onTouchEnd={handleLeave}
-          rowPosition={2}
-        >
+        <HoverContentLeft onMouseLeave={handleLeave} rowPosition={2}>
           <PurpleCircleLeft />
           <DesignText>Metal</DesignText>
-          <HoverButton to={"/dept_detail/Metal"}>
+          <HoverButton
+            to={"/dept_detail/Metal"}
+            onClick={(e) => e.stopPropagation()}
+          >
             <div className="hover-bg" />
-
             <div className="hover-text">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -135,49 +81,18 @@ const TitleSection = () => {
         </HoverContentLeft>
       )}
 
-      {hoveredLetter === "a" && (
-        <HoverContentRight
-          onMouseLeave={handleLeave}
-          onTouchEnd={handleLeave}
-          rowPosition={1}
-          isCraft={false}
-        >
-          <PurpleCircleRight />
-          <SculptureText>sculpture</SculptureText>
-          <HoverButton to={"/dept_detail/Sculpture"}>
-            <div className="hover-bg" />
-            <div className="hover-text">
-              <span className="dept-name">조소과</span>
-              <span className="cta"> 작품 보러가기</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={window.innerWidth > 768 ? 38 : 18}
-                height="26"
-                viewBox="0 0 38 26"
-                fill="none"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M22.7989 0.512563C23.4556 -0.170854 24.5205 -0.170854 25.1773 0.512563L36.95 12.7626C37.6068 13.446 37.6068 14.554 36.95 15.2374L25.1773 27.4874C24.5205 28.1709 23.4556 28.1709 22.7989 27.4874C22.1421 26.804 22.1421 25.696 22.7989 25.0126L31.7005 15.75H2.12445C1.1956 15.75 0.442627 14.9665 0.442627 14C0.442627 13.0335 1.1956 12.25 2.12445 12.25H31.7005L22.7989 2.98744C22.1421 2.30402 22.1421 1.19598 22.7989 0.512563Z"
-                  fill="#FFF"
-                />
-              </svg>
-            </div>
-          </HoverButton>
-        </HoverContentRight>
-      )}
-
       {hoveredLetter === "u" && (
         <HoverContentRight
           onMouseLeave={handleLeave}
-          onTouchEnd={handleLeave}
           rowPosition={2}
           isCraft={true}
         >
           <PurpleCircleRight />
           <CraftText>Ceramic</CraftText>
-          <HoverButton to={"/dept_detail/Ceramic"}>
+          <HoverButton
+            to={"/dept_detail/Ceramic"}
+            onClick={(e) => e.stopPropagation()}
+          >
             <div className="hover-bg" />
             <div className="hover-text">
               <span className="dept-name">도예전공</span>
