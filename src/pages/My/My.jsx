526fc4b5f719620be31e03af24df65cb1641251f
Daeyu <paull07@snu.ac.kr>
2024-11-28 10:48:37 +0900

    [Fix] 결제 승인 시각, 취소/환불 로직
    결제 승인 시각 표현, 환불신청여부 State 관리

--- src/pages/My/My.jsx
+++ src/pages/My/My.jsx
@@ -173,6 +173,7 @@ function My() {
                           onClick={() =>
                             navigate(`/payment-info/${item.item_id}`)
                           }
+                          style={{backgroundColor:'dimgray'}}
                         >
                           결제 정보
                         </RefundButton>
@@ -186,7 +187,7 @@ function My() {
                             onClick={() => handleRefundNavigation(item.item_id)}
                             disabled={item.refund}
                             style={{
-                              backgroundColor: item.refund ? "gray" : "#ff4d4f",
+                              backgroundColor: item.refund ? "gray" : "transparent",
                               cursor: item.refund ? "not-allowed" : "pointer",
                             }}
                           >
@@ -222,6 +223,7 @@ function My() {
                           onClick={() =>
                             navigate(`/payment-info/${item.item_id}`)
                           }
+                          style={{backgroundColor:'dimgray'}}
                         >
                           결제 정보
                         </RefundButton>
@@ -235,7 +237,7 @@ function My() {
                             onClick={() => handleRefundNavigation(item.item_id)}
                             disabled={item.refund}
                             style={{
-                              backgroundColor: item.refund ? "gray" : "#ff4d4f",
+                              backgroundColor: item.refund ? "gray" : "transparent",
                               cursor: item.refund ? "not-allowed" : "pointer",
                             }}
                           >
